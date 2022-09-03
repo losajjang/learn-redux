92ebd15c2bda8cf1ad13229bf786f5b94b258fe5
losajjang <losajjang@naver.com>
2022-09-03 15:30:40 +0900

    리덕스 개발자도구 적용하기

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import {legacy_createStore as createStore} from 'redux';
 import rootReducer from './modules';
 import {Provider} from 'react-redux';
+import { composeWithDevTools } from '@redux-devtools/extension'; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
+//composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
